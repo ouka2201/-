901124f370cf5e7e91adc28fc34f2ea441a9488a
ouka2201 <61953493+ouka2201@users.noreply.github.com>
2020-04-16 16:40:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import requests
+import json
+ 
+webhook_url = 'https://discordapp.com/api/webhooks/700232080885284904/ppHjyZ98WH7mgtHk7K-NCZ9e-udgzsJ7BYDPr0Vh5kYWiNiz3uUBM_rukslWXIVzTWUc'
 
+
+
+
+
+
+requests.post(webhook_url,json.dumps(main_content),headers={'Content-Type': 'application/json'})
+  
+  
+  
+  
