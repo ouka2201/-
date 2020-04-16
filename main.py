ce3b0d966b8c5c7f1209365796071faeed5f1671
ouka2201 <61953493+ouka2201@users.noreply.github.com>
2020-04-16 16:50:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
 import json
+import os
  
-webhook_url = 'https://discordapp.com/api/webhooks/700232080885284904/ppHjyZ98WH7mgtHk7K-NCZ9e-udgzsJ7BYDPr0Vh5kYWiNiz3uUBM_rukslWXIVzTWUc'
+webhook_url = os.environ['WEBHOOK_URL']
 
 
 
